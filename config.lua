5e27379bc9f61ca51145305ae05f02492687bd90
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-03-31 14:01:52 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,3 @@
-Config = {}
-
 Config = {
     Framework = 'QBCore', -- 'QBCore' or 'ESX', if using Qbox set to 'QBcore'
     UseOxLib = true,      -- Set to true to use ox_lib's lib.onCache instead of checking every five seconds
