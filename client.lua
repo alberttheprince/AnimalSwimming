87f46f570031941e270b0468486233fa01cc29e5
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-03-31 18:00:09 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,59 +1,91 @@
-local function IsPlayerAnimal()
-    local ped = PlayerPedId()
+local isAnimal = false
+local playerPed
+
+local function IsPedAnimal(ped)
     return GetPedType(ped) == 28
 end
 
 local function HandleAnimalSwim()
-    local ped = PlayerPedId()
+    while true do
+        Wait(200)
 
-    CreateThread(function()
-        while true do
-            ped = PlayerPedId()
-            
-            if not IsPlayerAnimal() then
-                Wait(5000)
-            else
-                Wait(200)
+        if not isAnimal then
+            return
+        end
 
-                SetPedDiesInWater(ped, false)
-                SetPedDiesInstantlyInWater(ped, false)
+        local ped = playerPed
 
-                if IsEntityInWater(ped) and not IsPedInAnyVehicle(ped, false) then
-                    SetPedCanRagdoll(ped, false)
+        SetPedDiesInWater(ped, false)
+        SetPedDiesInstantlyInWater(ped, false)
 
-                    local maxHealth = GetEntityMaxHealth(ped)
-                    if GetEntityHealth(ped) < maxHealth then
-                        SetEntityHealth(ped, maxHealth)
-                    end
+        if IsEntityInWater(ped) and not IsPedInAnyVehicle(ped, false) then
+            SetPedCanRagdoll(ped, false)
 
-                    local submergedLevel = GetEntitySubmergedLevel(ped)
-                    local pedCoords = GetEntityCoords(ped)
-                    local _, waterHeight = GetWaterHeight(pedCoords.x, pedCoords.y, pedCoords.z)
-                    local waterDepth = waterHeight - pedCoords.z
+            local maxHealth = GetEntityMaxHealth(ped)
+            if GetEntityHealth(ped) < maxHealth then
+                SetEntityHealth(ped, maxHealth)
+            end
 
-                    if submergedLevel > 0.25 then
-                        if waterDepth <= 0.5 then
-                            ApplyForceToEntity(ped, 1, 0.0, 0.0, 0.6, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
-                        else
-                            local force = math.min(0.4 * (submergedLevel - 0.25), 0.2)
-                            ApplyForceToEntity(ped, 1, 0.0, 0.0, force, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
-                        end
+            local submergedLevel = GetEntitySubmergedLevel(ped)
+            local pedCoords = GetEntityCoords(ped)
+            local _, waterHeight = GetWaterHeight(pedCoords.x, pedCoords.y, pedCoords.z)
+            local waterDepth = waterHeight - pedCoords.z
 
-                        if pedCoords.z > waterHeight + 0.1 then
-                            ApplyForceToEntity(ped, 1, 0.0, 0.0, -0.1, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
-                        end
-
-                        if pedCoords.z < waterHeight - 0.1 then
-                            ApplyForceToEntity(ped, 1, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
-                        end
-
-                        local currentRotation = GetEntityRotation(ped, 2)
-                        SetEntityRotation(ped, currentRotation.x + 0.2, currentRotation.y, currentRotation.z, 2, true)
-                    end
+            if submergedLevel > 0.25 then
+                if waterDepth <= 0.5 then
+                    ApplyForceToEntity(ped, 1, 0.0, 0.0, 0.6, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
                 else
-                    SetPedCanRagdoll(ped, true)
+                    local force = math.min(0.4 * (submergedLevel - 0.25), 0.2)
+                    ApplyForceToEntity(ped, 1, 0.0, 0.0, force, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
                 end
+
+                if pedCoords.z > waterHeight + 0.1 then
+                    ApplyForceToEntity(ped, 1, 0.0, 0.0, -0.1, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
+                end
+
+                if pedCoords.z < waterHeight - 0.1 then
+                    ApplyForceToEntity(ped, 1, 0.0, 0.0, 0.2, 0.0, 0.0, 0.0, 0, true, true, true, false, true)
+                end
+
+                local currentRotation = GetEntityRotation(ped, 2)
+                SetEntityRotation(ped, currentRotation.x + 0.2, currentRotation.y, currentRotation.z, 2, true)
+            end
+        else
+            SetPedCanRagdoll(ped, true)
+        end
+    end
+end
+
+local function CheckPed(ped)
+    if IsPedAnimal(ped) then
+        if not isAnimal then
+            isAnimal = true
+            playerPed = ped
+            CreateThread(HandleAnimalSwim)
+        end
+    else
+        isAnimal = false
+    end
+end
+
+-- Check for ox_lib, otherwise use a manual loop
+if lib and lib.onCache then
+    lib.onCache('ped', CheckPed)
+else
+    CreateThread(function()
+        local lastPed = nil
+        while true do
+            Wait(1000) -- Check every second
+            local ped = PlayerPedId()
+            if ped ~= lastPed then
+                lastPed = ped
+                CheckPed(ped)
             end
         end
     end)
 end
+
+-- Initial ped check
+CreateThread(function()
+    CheckPed(PlayerPedId())
+end)
