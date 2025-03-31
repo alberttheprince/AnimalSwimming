e58df9724e4379d8162b8286b3d24caf3c475b01
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-03-31 14:52:15 -0400

    Delete config.lua

--- config.lua
+++
@@ -1,5 +0,0 @@
-Config = {
-    Framework = 'QBCore', -- 'QBCore' or 'ESX', if using Qbox set to 'QBcore'
-    UseOxLib = true,      -- Set to true to use ox_lib's lib.onCache instead of checking every five seconds
-    Debug = false         -- Enable debug prints
-}
