9e589dfff47bab403937705cafd9b28afd3ee252
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-03-31 13:53:10 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,15 @@
-fx_version 'bodacious'
+fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
 
 author 'Cloutmatic'
 description 'Animal Swimming Script'
 version '1.0.0'
 
 client_script 'client.lua'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
